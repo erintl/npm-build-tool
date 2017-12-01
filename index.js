f025368b4838214f132b7f55903c2d45cc1fd561
Tim Erin <erintl@gmail.com>
2017-11-30 21:03:59 -0500

    Initial project setup.

---
+++ index.js
@@ -0,0 +1,11 @@
+var Koa = require('koa');
+var app = new Koa();
+module.exports = app;
+
+app.use(function(ctx) {
+  ctx.body = 'Koa says hi!';
+});
+
+var port = process.env.PORT || (process.argv[2] || 3000);
+app.listen(port);
+console.log(`Application started.  Listening on port: ${port}`);
